70cc2cfbbca2938a9e506850151dfd1be4966854
AurelienNioche <nioche.aurelien@gmail.com>
2018-02-27 00:23:08 +0100

    first commit

--- game/tutorial/management.py
+++
@@ -1,6 +0,0 @@
-from game.models import Players
-
-
-def get_tutorial_progression(player_id):
-    tutorial_progression = Players.objects.get(player_id=player_id).tutorial_progression
-    return tutorial_progression
