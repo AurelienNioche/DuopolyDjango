aaa5eb4b7410f5c929b6c21902110b35c8c48624
getz <basilegarcia@gmail.com>
2018-03-06 18:40:45 +0100

    merge

--- adminbase/settings.py
+++ adminbase/settings.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = 'adminbase.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DuopolyTest',
+        'NAME': 'DuopolyRefactor',
         'USER': 'dasein',
         'PASSWORD': '',
         'HOST': '',
@@ -84,7 +84,7 @@ if 'test' in sys.argv:
 
     DATABASES['default'] = {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DuopolyTest',
+        'NAME': 'DuopolyRefactor',
         'USER': 'dasein',
         'PASSWORD': '',
         'HOST': '',
