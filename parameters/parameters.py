2ad1d061f643b148d521ece74ababdb6771c769b
getz <basilegarcia@gmail.com>
2018-03-06 16:26:40 +0100

    param

--- parameters/parameters.py
+++ parameters/parameters.py
@@ -21,5 +21,3 @@ logs_path = os.getcwd() + "/log/"
 
 
 auto_reply_msg = "Hi! Thanks for joining the experiment! Unfortunately we're not available "\
-             "for the moment. We are French and in France it is currently {}. We hope that you'll enjoy "\
-             "the game. Good luck! PS: In case you forgot, the survey code is 999."
