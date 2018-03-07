8d9b8a7ac65f2d8735c06df42146b5634adce443
getz <basilegarcia@gmail.com>
2018-03-07 10:20:40 +0100

    merge

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -29,11 +29,21 @@ def main():
 
 
 def get_rooms():
-    rooms_025 = Room.objects.filter(opened=True, missing_players=2, radius=0.25)
-    rooms_05 = Room.objects.filter(opened=True, missing_players=2, radius=0.5)
 
-    print("N opened room 025:", rooms_025.count())
-    print("N opened room 05:", rooms_05.count())
+    rooms_025 = Room.objects.filter(opened=True, missing_players=2, radius=0.25).count()
+    rooms_05 = Room.objects.filter(opened=True, missing_players=2, radius=0.5).count()
+    rooms_025_done = Room.objects.filter(radius=0.25, state="end").count()
+    rooms_05_done = Room.objects.filter(radius=0.5, state="end").count()
+
+    print()
+    print("**************************************")
+    print("N opened room 025:", rooms_025)
+    print("N opened room 05:", rooms_05)
+    print("**************************************")
+    print("N validated room 025:", rooms_025_done)
+    print("N validated room 05:", rooms_05_done)
+    print("**************************************")
+    print()
 
 
 get_rooms()
