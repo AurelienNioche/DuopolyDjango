a59933f700e40d1573ef1dd8b9ab53eb570c9cbe
getz <basilegarcia@gmail.com>
2018-03-09 13:32:15 +0100

    merge

--- messenger/management.py
+++ messenger/management.py
@@ -19,7 +19,8 @@ def get_all_messages_from_user(user):
 
 def get_all_users():
 
-    users = User.objects.order_by("registered", "room_id", "username")
+    users = User.objects.order_by("registered", "room_id", "username")\
+        .only("tutorial_progression", "state", "registered", "connected", "username", "deserter")
     user_list = []
 
     # We sort users by room ids
