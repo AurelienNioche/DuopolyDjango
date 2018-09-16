4d1401f1a02f274a3e44ef5bf7aee7f5c231a9a6
getz <basilegarcia@gmail.com>
2018-09-16 21:10:26 +0200

    update

--- messenger/urls.py
+++ messenger/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = "messenger"
 urlpatterns = [
+    url(r'^duopoly-admin/messenger/$', views.MessengerClientSide.messenger),
     url(r'^messenger/$', views.MessengerClientSide.messenger),
     url(r'^messenger_view/$', views.MessengerView.as_view(), name='messenger_view'),
     url(r'^refresh_messenger/$', views.MessengerView.as_view())
